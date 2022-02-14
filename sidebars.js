eab6a9892c1b78f00d7f5ff2dde342cf22bf87bd
Stephanie Tremblay <stephanie.tremblay@botpress.com>
2022-02-14 11:03:41 -0500

    reworked the sidebar

--- sidebars.js
+++ sidebars.js
@@ -389,14 +389,16 @@ module.exports = {
                 'chatbot-management/chatbot-analytics/custom-analytics',
             ],
             },
-            {
-              type: 'category',
-              collapsible: true,
-              label: 'Language Understanding',
-              items: [
-                'chatbot-management/language-understanding/misunderstood',
-              ],
-        },
+          ],
+          },
+          {
+          type: 'category',
+          collapsible: false,
+          label: 'Language Understanding',
+          items: [
+            'chatbot-management/language-understanding/misunderstood',
+          ],
+          },
             {
             type: 'category',
             collapsible: false,
@@ -423,25 +425,18 @@ module.exports = {
               ],
             },
         ], 
-        },
-      ],
       enterpriseSidebar: [
         {
         type: 'category',
         collapsible: false,
-        label: 'Enterprise',
+        label: 'Licensing',
         items: [
-          {
-            type: 'category',
-            collapsible: true,
-            label: 'Licensing',
-            items: [
-              'enterprise/licensing/enterprise-licensing',
+          'enterprise/licensing/enterprise-licensing',
           ],
           },
           {
             type: 'category',
-            collapsible: true,
+            collapsible: false,
             label: 'User Management and Security',
             items: [
               {
@@ -478,7 +473,7 @@ module.exports = {
       },
           {
           type: 'category',
-          collapsible: true,
+          collapsible: false,
           label: 'Server and CICD Management',
           items: [
             'enterprise/server-and-cicd-management/production-checklist',
@@ -514,7 +509,5 @@ module.exports = {
             ],
           },
       ], 
-      },
-    ],
-    };
+      };
 
