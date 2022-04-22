115cec8712892264987530ccaeddcc8cfd7c0c02
Daehli Nadeau-Otis <daehlinadeauotis@gmail.com>
2022-04-22 17:22:54 -0400

    Added sidebar openbook

---
+++ sidebar/openbook.js
@@ -0,0 +1,36 @@
+module.exports = [
+    {
+      type: "category",
+      collapsible: false,
+      label: "Overview",
+      items: [
+        "openbook/what-is-openbook",
+        "openbook/getting-started",
+      ]
+    },
+    {
+      type: "category",
+      collapsible: true,
+      label: "Lesson",
+      items: [
+          'openbook/lessons/creating-your-first-book'
+      ]
+    },
+    {
+      type: "category",
+      collapsible: false,
+      label: "How-to's",
+      items: [
+          'openbook/how-tos/create-book'
+      ]
+    },
+    {
+      type: "category",
+      collapsible: false,
+      label: "Reference",
+      items: [
+          'openbook/reference/api'
+      ]
+    }
+  ]
+  
