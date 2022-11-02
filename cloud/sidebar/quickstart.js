af4d5209221113fde4eb3a20de9e17becfcde5a5
Jesse Hachey Blanchet <59566773+hacheybj@users.noreply.github.com>
2022-11-02 12:03:24 -0400

    Adding cloud

---
+++ cloud/sidebar/quickstart.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    type: "category",
+    collapsible: false,
+    label: "Quickstart",
+    items: [
+      "quickstart/quickstart", //,
+      // {
+      //   type: "category",
+      //   collapsible: true,
+      //   label: "Quickstart",
+      //   items: [
+      //     "overview/quickstart/installation",
+      //     "overview/quickstart/building-a-bot",
+      //     "overview/quickstart/admin-dashboard",
+      //     "overview/quickstart/conversation-studio",
+      //   ],
+      // },
+      // "overview/features",
+    ],
+  }, //,
+  // {
+  //   type: 'category',
+  //   label: 'Cloud',
+  //   collapsible: true,
+  //   items: [
+  //     "overview/cloud/getting-started",
+  //   ]
+  // }
+]
