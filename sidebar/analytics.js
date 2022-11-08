5840700d36a87674af2b028c962ec0353dd103a7
Bassam Tantawi <89806971+bassamtantawi-botpress@users.noreply.github.com>
2022-11-08 22:51:47 +0200

    Update analytics.js

--- sidebar/analytics.js
+++ sidebar/analytics.js
@@ -1,5 +1,10 @@
 module.exports = [
-   {
+  // Internal link
+    {
+      type: 'link',
+      label: 'Home', // The link label
+      href: '/analytics/analytics#selected-period', // The internal path
+    }, {
     type: "category",
     collapsible: false,
     label: "Analytics",
