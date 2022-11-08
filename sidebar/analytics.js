d0416a7c27ba8df92742e9883657894a2bf7c8bc
Bassam Tantawi <89806971+bassamtantawi-botpress@users.noreply.github.com>
2022-11-08 22:47:47 +0200

    Update analytics.js

--- sidebar/analytics.js
+++ sidebar/analytics.js
@@ -1,9 +1,16 @@
 module.exports = [
-  {
+   {
+    type: "category",
+    collapsible: false,
+    label: "Analytics",
+    items: ["analytics/analytics"],
+  },
+   {
       type: 'html',
       value: '<a src="#selected-period" alt="Selected Period" >Selected Period</a>', // The HTML to be rendered
       defaultStyle: true, // Use the default menu item styling
-    },{
+    },
+   {
     type: "category",
     collapsible: false,
     label: "Analytics",
