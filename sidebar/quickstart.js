02088aa854caec5f57d34dd7ebcae75ec23115e9
rohankokkulabp <rohan.kokkula@botpress.com>
2022-11-22 17:34:08 +0530

    updated quickstart

--- sidebar/quickstart.js
+++ sidebar/quickstart.js
@@ -3,31 +3,13 @@ module.exports = [
     type: "category",
     collapsible: false,
     label: " ",
-    items: ["quickstart/quickstart"],
-  },
-  {
-    type: "link",
-    label: "1. Login & Signup",
-    href: "/login-signup",
-  },
-  {
-    type: "link",
-    label: "2. Download Botpress Studio",
-    href: "/download-studio",
-  },
-  {
-    type: "link",
-    label: "3. Create a chatbot on Studio",
-    href: "/create-chatbot",
-  },
-  {
-    type: "link",
-    label: "4. Publishing your chatbot",
-    href: "/publishing-chatbot",
-  },
-  {
-    type: "link",
-    label: "5. Testing your chatbot",
-    href: "/testing",
+    items: [
+      "quickstart/quickstart",
+      "quickstart/login",
+      "quickstart/download-studio",
+      "quickstart/create-your-first-chatbot",
+      "quickstart/publishing-your-chatbot",
+      "quickstart/testing-your-chatbot",
+    ],
   },
 ]
