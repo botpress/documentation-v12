b9057087afb35c1b4f199927683f1dd52622a883
shubham7799 <sssuryvanshi7799@gmail.com>
2022-11-22 17:06:06 +0530

    new quickstart

--- sidebar/quickstart.js
+++ sidebar/quickstart.js
@@ -3,35 +3,31 @@ module.exports = [
     type: "category",
     collapsible: false,
     label: " ",
-    items: [
-      "quickstart/quickstart",
-    ],
-  }, 
-  {
-    type: 'link',
-    label: '1. Login & Signup', 
-    href: '/#login',
+    items: ["quickstart/quickstart"],
   },
   {
-    type: 'link',
-    label: '2. Download Botpress Studio', 
-    href: '/#download-studio',
+    type: "link",
+    label: "1. Login & Signup",
+    href: "/login-signup",
   },
   {
-    type: 'link',
-    label: '3. Create a chatbot on Studio', 
-    href: '/#create-chatbot',
+    type: "link",
+    label: "2. Download Botpress Studio",
+    href: "/download-studio",
   },
   {
-    type: 'link',
-    label: '4. Publishing your chatbot', 
-    href: '/#publishing-chatbot',
+    type: "link",
+    label: "3. Create a chatbot on Studio",
+    href: "/create-chatbot",
   },
   {
-    type: 'link',
-    label: '5. Testing your chatbot', 
-    href: '/#testing',
+    type: "link",
+    label: "4. Publishing your chatbot",
+    href: "/publishing-chatbot",
   },
-  
+  {
+    type: "link",
+    label: "5. Testing your chatbot",
+    href: "/testing",
+  },
 ]
-
